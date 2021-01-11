4208dafb7ee39173233e6bc14fe09266d846a083
Yannick Aiguier <yannick.aiguier@gmail.com>
2021-01-11 14:44:47 +0100

    Etape 3 sécurisée

--- includes/header.php
+++ includes/header.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?=$metaDescription?>">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css">
     <title><?=$metaTitle?></title>
 </head>
 
 <body>
 <header>
     <div>Yannick Aiguier</div>
-    <div id="photo"><img src="images/photo.jpg" alt="Une photo de Yannick"></div>
+    <div id="photo"><img src="../images/photo.jpg" alt="Une photo de Yannick"></div>
 </header>
 <nav>
-    <a href="index.php?page=cv">Mon CV</a> | <a href="index.php?page=hobby">Mon Hobby</a> | <a href="index.php?page=contact">Me
+    <a href="../index.php?page=cv">Mon CV</a> | <a href="../index.php?page=hobby">Mon Hobby</a> | <a href="../index.php?page=contact">Me
         contacter</a><br>
 </nav>
--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 $metaTitle = 'Le CV de Yannick';
 $metaDescription = 'Actuellement en formation au Campus Numérique de Valence comme technicien développeur';
-if (isset($_GET['page']) )// nom de la page renseigné
-{
-    $page = $_GET['page'];
-    if ($page == 'cv') {
-        // rien à faire
-    } elseif ($page == 'hobby') {
-        $metaTitle = 'Le hobby de Yannick';
-        $metaDescription = 'Ma passion pour le badminton, un sport très physique et ludique';
-    } elseif ($page == 'contact') {
-        $metaTitle = 'Contacter Yannick';
-        $metaDescription = 'Envoyez-moi un message';
-    } else {
-        $page = '404';
+if (isset($_GET['page'])) { // nom de la page renseigné
+    $page = filter_var($_GET['page'], FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_NULL_ON_FAILURE);
+    if ($page)
+    {
+        if ($page == 'cv') {
+            // rien à faire
+        } elseif ($page == 'hobby') {
+            $metaTitle = 'Le hobby de Yannick';
+            $metaDescription = 'Ma passion pour le badminton, un sport très physique et ludique';
+        } elseif ($page == 'contact') {
+            $metaTitle = 'Contacter Yannick';
+            $metaDescription = 'Envoyez-moi un message';
+        } else {
+            $page = '404';
+        }
     }
-} else {
+} else { // page cv par défaut
     $page = 'cv';
-
 }
-include 'header.php';
-include 'pages/'.$page.'.php';
-include 'footer.php';
+require 'includes/header.php';
+require 'pages/'.$page.'.php';
+require 'includes/footer.php';
