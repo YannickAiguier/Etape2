14993339d58e8b995c2bc7dc6b84efe341f445e2
Yannick Aiguier <yannick.aiguier@gmail.com>
2021-01-12 16:18:11 +0100

    4.4 formulaire complet, full vérifications

--- includes/formulaire.php
+++ includes/formulaire.php
@@ -1,5 +1,5 @@
 <?php
-$erreur = [
+$result_form = $erreur = [
     "civilite" => '',
     "nom-utilisateur" => '',
     "prenom-utilisateur" => '',
@@ -7,17 +7,97 @@ $erreur = [
     "raison" => '',
     "msg-utilisateur" => ''
 ];
+$civilite_choisie = [
+    'M'     => '',
+    'Mme'   => ''
+];
+$raison_choisie = [
+    'emploi'    => '',
+    'infos'     => '',
+    'presta'    => ''
+];
+/*$result_form = [
+    "civilite" => '',
+    "nom-utilisateur" => '',
+    "prenom-utilisateur" => '',
+    "mail-utilisateur" => '',
+    "raison" => '',
+    "msg-utilisateur" => ''
+];*/
+/*$args = array(
+    'civilite'          => FILTER_SANITIZE_STRING,
+    'nom-utilisateur'   => FILTER_SANITIZE_STRING,
+    'prenom-utilisateur'=> FILTER_SANITIZE_STRING,
+    'mail-utilisateur'  => FILTER_VALIDATE_EMAIL,
+    'raison'            => FILTER_SANITIZE_STRING,
+    'msg-utilisateur'   => FILTER_SANITIZE_STRING
+);*/
+
 $erreur_presente = false;
 if (!empty($_POST)) {
-    foreach($_POST as $cle => $valeur) {
-        if ($valeur == '') {
-            $erreur[$cle] = "Ce champ doit être renseigné !";
-            $erreur_presente = true;
+    $result_form = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    if ($result_form['civilite'] != "M." && $result_form['civilite'] != "Mme") {
+        $erreur['civilite'] = "Merci de choisir votre civilité";
+        $erreur_presente = true;
+    } else {
+        switch ($result_form['civilite']){
+            case "M.":
+                $civilite_choisie['M'] = "selected";
+                break;
+            case "Mme":
+                $civilite_choisie['Mme'] = "selected";
+                break;
         }
     }
+    if ($result_form['nom-utilisateur'] == '') {
+        $erreur['nom-utilisateur'] = "Votre nom est nécessaire";
+        $erreur_presente = true;
+    }
+    if ($result_form['prenom-utilisateur'] == '') {
+        $erreur['prenom-utilisateur'] = "Votre prénom est nécessaire";
+        $erreur_presente = true;
+    }
+    if ($result_form['mail-utilisateur'] == '') {
+        $erreur['mail-utilisateur'] = "Merci de renseigner votre adresse email";
+        $erreur_presente = true;
+    } else {
+        // validation de l'adresse mail
+        $mail_valide = filter_var($result_form['mail-utilisateur'], FILTER_VALIDATE_EMAIL);
+        if (!$mail_valide) {
+            $erreur['mail-utilisateur'] = "Votre adresse mail n'est pas valide";
+            $erreur_presente = true;
+        } else {
+            $result_form['mail-utilisateur'] = $mail_valide;
+        }
+    }
+    if (!isset($result_form['raison'])) {
+        $erreur['raison'] = "Choisissez un motif de contact !";
+        $erreur_presente = true;
+    } elseif ($result_form['raison'] != "emploi" && $result_form['raison'] != "infos" && $result_form['raison'] != "presta") {
+        $erreur['raison'] = "Pour quelle raison me contactez-vous ?";
+        $erreur_presente = true;
+    } else {
+        switch ($result_form['raison']){
+            case "emploi":
+                $raison_choisie['emploi'] = "checked";
+                break;
+            case "infos":
+                $raison_choisie['infos'] = "checked";
+                break;
+            case "presta":
+                $raison_choisie['presta'] = "checked";
+                break;
+        }
+    }
+    if (strlen($result_form['msg-utilisateur']) < 5 ) {
+        $erreur['msg-utilisateur'] = "Merci de taper un message plus explicite";
+        $erreur_presente = true;
+    }
+
     if (!$erreur_presente) {
-        $sanitized = implode(" ", $_POST);
+        $sanitized = implode(" ", $result_form);
         $filename = "contact/contact_".date("Y-m-d-H-i-s").".txt";
         file_put_contents($filename, $sanitized, FILE_APPEND);
+        $result_form = array_fill(0, count($result_form), '');
     }
 }
--- pages/contact.php
+++ pages/contact.php
@@ -6,61 +6,57 @@
         ?>
         <div id="formulaire">
             <h1>Formulaire de contact</h1>
-            <!--<form action="https://httpbin.org/post" method="POST">-->
                 <form action="/?page=contact" method="POST">
                 <div class="box">
-                    <?php
-                    echo $erreur['civilite']."<br>";
-                    ?>
+                    <?=$erreur['civilite']?>
+                    <br>
                     <label class="lab" for="choix">Civilité :</label>
-                    <select name="civilite" id="choix">
+                    <select name="civilite" id="choix" value="Mme">
                         <option value="">--Choisissez--</option>
-                        <option value="M">M.</option>
-                        <option value="Mme">Mme</option>
+                        <option value="M." <?=$civilite_choisie['M']?>>M.</option>
+                        <option value="Mme" <?=$civilite_choisie['Mme']?>>Mme</option>
                     </select>
                 </div>
                 <div class="box" >
-                    <?php
-                    echo $erreur['nom-utilisateur']."<br>";
-                    ?>
+                    <?= $erreur['nom-utilisateur'] ?>
+                    <br>
                     <label class="lab" for="formnom">Nom :</label>
-                    <input type="text" id="formnom" name="nom-utilisateur">
+                    <input type="text" id="formnom" name="nom-utilisateur" value="<?=$result_form['nom-utilisateur']?>">
                 </div>
                 <div class="box" >
-                    <?php
-                    echo $erreur['prenom-utilisateur']."<br>";
-                    ?>
+                    <?=$erreur['prenom-utilisateur']?>
+                    <br>
                     <label class="lab" for="formprenom">Prénom :</label>
-                    <input type="text" id="formprenom" name="prenom-utilisateur">
+                    <input type="text" id="formprenom" name="prenom-utilisateur" value="<?=$result_form['prenom-utilisateur']?>">
                 </div>
                 <div class="box" >
-                    <?php
-                    echo $erreur['mail-utilisateur']."<br>";
-                    ?>
+                    <?=$erreur['mail-utilisateur']?>
+                    <br>
                     <label class="lab" for="formmail">E-mail :</label>
-                    <input type="email" id="formmail" name="mail-utilisateur">
+                    <input type="text" id="formmail" name="mail-utilisateur" value="<?=$result_form['mail-utilisateur']?>">
                 </div>
                 <div class="box" id="radios">
+                    <?=$erreur['raison']?>
+                    <br>
                     <p> Raison du contact :</p>
                     <div>
-                        <input type="radio" id="emploi" name="raison" value="emploi">
+                        <input type="radio" id="emploi" name="raison" value="emploi" <?=$raison_choisie['emploi']?>>
                         <label for="emploi">Proposition d'emploi</label>
                     </div>
                     <div>
-                        <input type="radio" id="infos" name="raison" value="infos" checked>
+                        <input type="radio" id="infos" name="raison" value="infos" <?=$raison_choisie['infos']?>>
                         <label for="infos">Demande d'informations</label>
                     </div>
                     <div>
-                        <input type="radio" id="presta" name="raison" value="presta">
+                        <input type="radio" id="presta" name="raison" value="presta" <?=$raison_choisie['presta']?>>
                         <label for="presta">Prestations</label>
                     </div>
                 </div>
                 <div class="box" >
-                    <?php
-                    echo $erreur['msg-utilisateur']."<br>";
-                    ?>
+                    <?=$erreur['msg-utilisateur']?>
+                    <br>
                     <label class="lab" for="formmsg">Message :</label>
-                    <textarea name="msg-utilisateur" id="formmsg" cols="30" rows="10"></textarea>
+                    <textarea name="msg-utilisateur" id="formmsg" cols="30" rows="10"><?=$result_form['msg-utilisateur']?></textarea>
                 </div>
                 <div class="box" id="bouton">
                     <button type="submit">Envoyer</button>
