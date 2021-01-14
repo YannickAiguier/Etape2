387ef6fa4f6280c72d9d3337e7f848b45b5a0f83
Yannick Aiguier <yannick.aiguier@gmail.com>
2021-01-14 13:59:46 +0100

    Refactor : utilisation de filter_input_array pour nettoyer/valider tous les chamos d'un coup (évite le problème de correction automatique de l'email)

--- includes/formulaire.php
+++ includes/formulaire.php
@@ -5,26 +5,31 @@ $result_form = $formErrors = [
     "prenom-utilisateur" => '',
     "mail-utilisateur" => '',
     "raison" => '',
-    "msg-utilisateur" => ''
-];
+    "msg-utilisateur" => ''];
 $civilite_choisie = [
     'M'     => '',
-    'Mme'   => ''
-];
+    'Mme'   => ''];
 $raison_choisie = [
     'emploi'    => '',
     'infos'     => '',
-    'presta'    => ''
-];
+    'presta'    => ''];
 $erreur_presente = false;
 
+$args = [
+    'civilite' => FILTER_SANITIZE_STRING,
+    'nom-utilisateur' => FILTER_SANITIZE_STRING,
+    'prenom-utilisateur' => FILTER_SANITIZE_STRING,
+    'mail-utilisateur' => FILTER_VALIDATE_EMAIL,
+    'raison' => FILTER_SANITIZE_STRING,
+    'msg-utilisateur' => FILTER_SANITIZE_STRING];
+
 if (!empty($_POST)) {
-    $result_form = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    $result_form = filter_input_array(INPUT_POST, $args);
     if ($result_form['civilite'] != "M." && $result_form['civilite'] != "Mme") {
         $formErrors['civilite'] = "Merci de choisir votre civilité";
         $erreur_presente = true;
     } else {
-        switch ($result_form['civilite']){
+        switch ($result_form['civilite']) {
             case "M.":
                 $civilite_choisie['M'] = "selected";
                 break;
@@ -41,48 +46,39 @@ if (!empty($_POST)) {
         $formErrors['prenom-utilisateur'] = "Votre prénom est nécessaire";
         $erreur_presente = true;
     }
-    if ($result_form['mail-utilisateur'] == '') {
-        $formErrors['mail-utilisateur'] = "Merci de renseigner votre adresse email";
+    if ($result_form['mail-utilisateur'] == false) {
+        $formErrors['mail-utilisateur'] = "Merci de renseigner une adresse email valide";
         $erreur_presente = true;
-    } else {
-        // validation de l'adresse mail
-        $mail_valide = filter_var($result_form['mail-utilisateur'], FILTER_VALIDATE_EMAIL);
-        if (!$mail_valide) {
-            $formErrors['mail-utilisateur'] = "Votre adresse mail n'est pas valide";
+        if (!isset($result_form['raison'])) {
+            $formErrors['raison'] = "Choisissez un motif de contact !";
+            $erreur_presente = true;
+        } elseif ($result_form['raison'] != "emploi" && $result_form['raison'] != "infos" && $result_form['raison'] != "presta") {
+            $formErrors['raison'] = "Pour quelle raison me contactez-vous ?";
             $erreur_presente = true;
         } else {
-            $result_form['mail-utilisateur'] = $mail_valide;
+            switch ($result_form['raison']) {
+                case "emploi":
+                    $raison_choisie['emploi'] = "checked";
+                    break;
+                case "infos":
+                    $raison_choisie['infos'] = "checked";
+                    break;
+                case "presta":
+                    $raison_choisie['presta'] = "checked";
+                    break;
+            }
         }
-    }
-    if (!isset($result_form['raison'])) {
-        $formErrors['raison'] = "Choisissez un motif de contact !";
-        $erreur_presente = true;
-    } elseif ($result_form['raison'] != "emploi" && $result_form['raison'] != "infos" && $result_form['raison'] != "presta") {
-        $formErrors['raison'] = "Pour quelle raison me contactez-vous ?";
-        $erreur_presente = true;
-    } else {
-        switch ($result_form['raison']){
-            case "emploi":
-                $raison_choisie['emploi'] = "checked";
-                break;
-            case "infos":
-                $raison_choisie['infos'] = "checked";
-                break;
-            case "presta":
-                $raison_choisie['presta'] = "checked";
-                break;
+        if (strlen($result_form['msg-utilisateur']) < 5) {
+            $formErrors['msg-utilisateur'] = "Merci de taper un message plus explicite";
+            $erreur_presente = true;
         }
-    }
-    if (strlen($result_form['msg-utilisateur']) < 5 ) {
-        $formErrors['msg-utilisateur'] = "Merci de taper un message plus explicite";
-        $erreur_presente = true;
-    }
 
-    if (!$erreur_presente) { // aucune erreur dans le formulaire, on peut l'envoyer
-        $sanitized = implode(" ", $result_form);
-        $filename = "contact/contact_".date("Y-m-d-H-i-s").".txt";
-        file_put_contents($filename, $sanitized, FILE_APPEND);
-        $result_form = array_fill(0, count($result_form), '');
-        header('Location: index.php?page=formulaire_ok');
+        if (!$erreur_presente) { // aucune erreur dans le formulaire, on peut l'envoyer
+            $sanitized = implode(" ", $result_form);
+            $filename = "contact/contact_" . date("Y-m-d-H-i-s") . ".txt";
+            file_put_contents($filename, $sanitized, FILE_APPEND);
+            $result_form = array_fill(0, count($result_form), '');
+            header('Location: index.php?page=formulaire_ok');
+        }
     }
 }
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 $metaTitle = 'Le CV de Yannick';
 $metaDescription = 'Actuellement en formation au Campus Numérique de Valence comme technicien développeur';
 $routes = [
-    'cv' => 'cv',
-    'hobby' => 'hobby',
-    'contact' => 'contact',
-    'formulaire_ok' => 'formulaire_ok',
-    '404' => '404'
+    'cv' => 'pages/cv',
+    'hobby' => 'pages/hobby',
+    'contact' => 'pages/contact',
+    'formulaire_ok' => 'pages/formulaire_ok',
+    '404' => 'pages/404'
 ];
 if (isset($_GET['page'])) { // nom de la page renseigné
     $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
@@ -17,5 +17,5 @@ if (isset($_GET['page'])) { // nom de la page renseigné
     $page = 'cv';
 }
 require 'includes/header.php';
-require 'pages/'.$routes[$page].'.php';
+require $routes[$page].'.php';
 require 'includes/footer.php';
